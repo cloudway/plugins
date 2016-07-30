1f5d26de8aba374420e5ef0a73457dcd7a301961
daniel <daniel.yuan@me.com>
2016-07-30 13:11:17 +0800

    增加Django插件

---
+++ django/template/mysite/urls.py
@@ -0,0 +1,28 @@
+""" URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/1.9/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.conf.urls import url, include
+    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
+"""
+
+from django.conf.urls import url
+from django.contrib import admin
+
+# Remove this when creating your own application.
+def index(request):
+    from django.http import HttpResponse
+    return HttpResponse("<html><body><h1>It works!</h1></body></html>")
+
+urlpatterns = [
+    url(r'^admin/', admin.site.urls),
+    url(r'^$', index, name='index'),
+]
