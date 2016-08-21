fbb6faa8655b61c326feb8263e9e42b1dfd6e1cf
daniel <daniel.yuan@me.com>
2016-08-21 12:51:47 +0800

    优化docker pull

--- pull-all.sh
+++ pull-all.sh
@@ -3,7 +3,8 @@
 for d in *; do
     if [ -f $d/manifest/plugin.yml ]; then
         img=$(sed -e '/^Base-Image:/!d' -e 's/^Base-Image: \(.*\)$/\1/' $d/manifest/plugin.yml)
-        docker pull $img
+        if ! docker inspect "$img" &>/dev/null; then
+            docker pull "$img"
+        fi
     fi
 done
-
